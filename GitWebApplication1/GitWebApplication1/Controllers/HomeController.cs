e036451d37341f527b336eef8df30533f5a5016d
Tianxs <Tianxs@TIANXS-PC>
2018-09-03 22:41:45 +0800

    1

--- GitWebApplication1/GitWebApplication1/Controllers/HomeController.cs
+++ GitWebApplication1/GitWebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace GitWebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            return View();//
+            return View();
         }
 
         public ActionResult About()
